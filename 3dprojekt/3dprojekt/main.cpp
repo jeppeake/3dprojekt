9ed8f336a4cc75fbd99f7c0b859e0df4300559c4
Tristan <tristan23@live.se>
2017-01-19 19:32:56 +0100

    changed name of test.cpp to main.cpp

--- 3dprojekt/3dprojekt/main.cpp
+++ 3dprojekt/3dprojekt/main.cpp
@@ -10,7 +10,11 @@ int main(void) {
 		return -1;
 
 	/* Create a windowed mode window and its OpenGL context */
-	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+	window = glfwCreateWindow(640, 
+							  480, 
+							  "Hello World", 
+							  NULL, 
+							  NULL);
 	if (!window) {
 		glfwTerminate();
 		return -1;
